bf0405b0c17c028998dd6f29dd746c4b3dd06f44
chrisbohne <christoph.bohne90@gmail.com>
2022-02-13 10:41:19 +0100

    Includ validation for having all the correct env variables using joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { MapModule } from './maps/map.module';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required,
+        DATABASE_URL: Joi.string().required,
+      }),
+      isGlobal: true,
+    }),
     MapModule,
     UserModule,
     AuthModule,
